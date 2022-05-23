cfb5c4fe59049daabb24a1bc53260e9d27cb117a
Temidayo325 <59534757+Temidayo325@users.noreply.github.com>
2022-05-23 23:38:07 +0100

    Setup of tailwind succesful"

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+       "./src/**/*.{html,ts}"
+ ],
+  theme: {
+    extend: {},
+  },
+  mode: 'jit',
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+}
