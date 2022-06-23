2b42d37818edac9ee43aa7606272bb5de7bcc74f
Temidayo325 <59534757+Temidayo325@users.noreply.github.com>
2022-06-23 06:08:01 +0100

    Updated the font accross the pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     extend: {
          backgroundImage: {
               'login-bg': 'src/app/assets/login.jpg'
-         }
+         },
+         fontFamily: {
+             'headline': 'Amaranth',
+             'word': 'Poppins'
+          }
     },
   },
   mode: 'jit',
